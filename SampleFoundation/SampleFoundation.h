65488e41e4b027f36a09272df0650a6aacb3cf37
Mike Daly <mdaly@l4mobile.com>
2015-01-08 12:05:29 -0800

    Initial Commit

---
+++ SampleFoundation/SampleFoundation.h
@@ -0,0 +1,13 @@
+//
+//  SampleFoundation.h
+//  SampleFoundation
+//
+//  Created by Mike Daly on 1/8/15.
+//  Copyright (c) 2015 Mike Daly. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SampleFoundation : NSObject
+
+@end
---
+++ SampleFoundation/SampleFoundation.m
@@ -0,0 +1,13 @@
+//
+//  SampleFoundation.m
+//  SampleFoundation
+//
+//  Created by Mike Daly on 1/8/15.
+//  Copyright (c) 2015 Mike Daly. All rights reserved.
+//
+
+#import "SampleFoundation.h"
+
+@implementation SampleFoundation
+
+@end
